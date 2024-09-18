f9b5c503520d8292520a0440a546a2d8a07a89d7
Breno <breno_mg10@hotmail.com>
2024-09-18 20:20:07 -0300

    feat: base main component

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -1,5 +1,32 @@
+import { LuTrash } from "react-icons/lu";
+
 import "@/styles/components/main.scss";
 
 export default function Main() {
-  return <div className="main-container">main</div>;
+  return (
+    <div className="main-container">
+      <main>
+        <h1>Suas tarefas de hoje</h1>
+
+        <ul className="tasks">
+          <li>
+            <p>Tasks</p>
+            <LuTrash />
+          </li>
+          <li>
+            <p>Tasks 1</p> <LuTrash />
+          </li>
+          <li>
+            <p>Tasks 2</p> <LuTrash />
+          </li>
+        </ul>
+        <h1>Tarefas finalizadas</h1>
+        <ul className="completTasks">
+          <li>
+            <p>completTasks</p> <LuTrash />
+          </li>
+        </ul>
+      </main>
+    </div>
+  );
 }
